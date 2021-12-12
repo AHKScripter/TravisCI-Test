764709c0d61342f61b7c3c0b855188d05679294a
AHKScripter <sergeiplugatyr@gmail.com>
2021-12-12 22:06:38 +0700

    Add files via upload

---
+++ TRPO_Functions.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <clocale>
+#include <stdlib.h>
+
+double sum(double a, double b) {
+    double r;
+    r = a + b;
+    return r;
+}
+
+double mynus(double a, double b) {
+    double r;
+    r = a - b;
+    return r;
+}
+
+double multiply(double a, double b) {
+    double r;
+    r = a * b;
+    return r;
+}
+double share(double a, double b) {
+    double r;
+    r = a / b;
+    return r;
+}
+
+double elevate(double a, double b) {
+    double r;
+    r = pow(a, b);
+    return r;
+}
---
+++ TRPO_Functions.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+double sum(double a, double b);
+double mynus(double a, double b);
+double multiply(double a, double b);
+double share(double a, double b);
+double elevate(double a, double b);
+
+
